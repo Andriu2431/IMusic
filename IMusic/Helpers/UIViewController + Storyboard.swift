1627e70b0c6e6ab6e6800c4353c06a1767bc2545
Andriy <andriy@MacBook-Pro-Admin.local>
2022-08-21 11:21:12 +0300

    SDWebImage

--- IMusic/Helpers/UIViewController + Storyboard.swift
+++ IMusic/Helpers/UIViewController + Storyboard.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIViewController {
     
-    //Метод буде шукати файл типу UIStoryboard
+    //Метод буде шукати файл типу UIStoryboard - тобто якщо є 2 однакових імені то ми шукаємо типу UIStoryboard
     class func loadFromStoryboard<T: UIViewController>() -> T {
         let name = String(describing: T.self)
         let storyboard = UIStoryboard(name: name, bundle: nil)
--- IMusic/Search/Cell/TrackCell.swift
+++ IMusic/Search/Cell/TrackCell.swift
@@ -5,8 +5,8 @@
 //  Created by Andriy on 20.08.2022.
 //
 
-import Foundation
 import UIKit
+import SDWebImage
 
 //Прротокол для того щоб реалізувати лише ті данні які в ньому записані
 protocol TrackCellViewModel {
@@ -30,10 +30,23 @@ class TrackCell: UITableViewCell {
         
     }
     
+    //Коли контейнери перевикористовуються
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        //Для того коли контейнер буде перевикористовуватись то фотки не буде - щоб не було багу з заміною фотки на іншу
+        trackImageView.image = nil
+    }
+    
     //Метод який буде заповнювати UIелементи даними
     func set(viewModel: TrackCellViewModel) {
         trackNameLabel.text = viewModel.trackName
         artistNameLabel.text = viewModel.artistName
         collectionNameLabel.text = viewModel.collectionName
+        
+        guard let url = URL(string: viewModel.iconUrlString ?? "") else { return }
+        
+        //Ця бібліотека хороша тому що вона зразу кешує фото та потім не загружає їх знов а бере з кешу
+        trackImageView.sd_setImage(with: url, completed: nil)
     }
 }
